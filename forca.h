78b301b80c883c5bc82e12a23889792ff3906354
IgornJ17 <39203490+IgornJ17@users.noreply.github.com>
2018-06-12 10:44:14 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,10 @@
+#define PALAVRA_SECRETA 30
+void adicionapalavra();
+void escolhapalavra();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+int enforcou();
+int ganhou();
+int chuteserrados();
